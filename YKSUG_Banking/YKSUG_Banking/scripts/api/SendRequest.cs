09eba49299a51d3175c1aa55a6d352318d1e90b2
Yauheni Shumsky <shumsky.yauheny@gmail.com>
2024-02-19 09:45:16 +0300

    Add api

---
+++ YKSUG_Banking/YKSUG_Banking/scripts/api/SendRequest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace YKSUG_Banking.scripts.api
+{
+    public class SendRequest
+    {
+        public static async Task<string> PostRequest(string url, object request, string token)
+        {
+            HttpClient client = new HttpClient();
+        
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        
+            var content = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url, content);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public static async Task<string> SendGetRequest(string url, string token)
+        {
+            HttpClient client = new HttpClient();
+        
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        
+            var response = await client.GetAsync(new Uri(url));
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
