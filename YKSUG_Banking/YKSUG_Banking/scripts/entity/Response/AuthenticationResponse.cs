9eda25b1d56c9bf93c293c022038a22e56cbea2b
Yauheni Shumsky <shumsky.yauheny@gmail.com>
2024-02-19 09:47:45 +0300

    Add Response

---
+++ YKSUG_Banking/YKSUG_Banking/scripts/entity/Response/AuthenticationResponse.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace YKSUG_Banking.scripts.entity.Response
+{
+    public class AuthenticationResponse
+    {
+        [JsonProperty(PropertyName="token")]
+        public string Token { get; set; }
+        [JsonProperty(PropertyName="role")]
+        public string Role { get; set; }
+    }
+}
---
+++ YKSUG_Banking/YKSUG_Banking/scripts/entity/Response/BuyBonusResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace YKSUG_Banking.scripts.entity.Response
+{
+    public class BuyBonusResponse
+    {
+        [JsonProperty(PropertyName="token")]
+        public string Token { get; set; }
+    }
+}
