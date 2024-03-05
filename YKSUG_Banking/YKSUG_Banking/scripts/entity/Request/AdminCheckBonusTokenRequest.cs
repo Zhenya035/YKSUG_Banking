d37ad3e14b25359bed6e4e0c27dfece2b46091af
Yauheni Shumsky <shumsky.yauheny@gmail.com>
2024-03-05 18:41:24 +0300

    Add Settings

---
+++ YKSUG_Banking/YKSUG_Banking/scripts/entity/Request/AdminCheckBonusTokenRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace YKSUG_Banking.scripts.entity.Request
+{
+    public class AdminCheckBonusTokenRequest
+    {
+        [JsonProperty(PropertyName="username")]
+        public string Username { get; set; }
+        [JsonProperty(PropertyName="token")]
+        public string Token { get; set; }
+
+    }
+}
