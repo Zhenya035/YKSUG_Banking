b6861b59493de83d0a2180653f42cbca698d092a
Yauheni Shumsky <shumsky.yauheni@gmail.com>
2024-03-13 21:35:34 +0300

    block orientation

--- YKSUG_Banking/YKSUG_Banking.Droid/MainActivity.cs
+++ YKSUG_Banking/YKSUG_Banking.Droid/MainActivity.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 
 namespace YKSUG_Banking.Droid
 {
-    [Activity(Label = "YKSUG_Banking", Theme = "@style/MainTheme", MainLauncher = true,
+    [Activity(ScreenOrientation = ScreenOrientation.Portrait, Label = "YKSUG_Banking", Theme = "@style/MainTheme", MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
